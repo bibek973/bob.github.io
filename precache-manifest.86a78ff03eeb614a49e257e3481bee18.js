2ca53ff02576a65404df77d39cde92ff1235c535
BI20020291 <bibek.prasad1@wipro.com>
2020-04-01 03:39:04 +0530

    Updates

---
+++ precache-manifest.86a78ff03eeb614a49e257e3481bee18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ecb54d3220260e32c81f358358820d",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  },
+  {
+    "revision": "79981b6ee770e20589f4",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.735c6169.chunk.css"
+  },
+  {
+    "revision": "e099ffd560b102cbaa0a",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.88298884.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TD-React-Challenge-Test-1/static/js/2.88298884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79981b6ee770e20589f4",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.ad342aeb.chunk.js"
+  },
+  {
+    "revision": "eb7d57b2eb9233038fa6",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime-main.43ab0407.js"
+  }
+]);
